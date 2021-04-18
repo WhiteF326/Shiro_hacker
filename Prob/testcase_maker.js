a659198df0f16ab0ce3e34cf5e70b2add3f983e6
WhiteF326 <Lume1a326.leno@gmail.com>
2021-04-18 10:40:33 +0900

    making

--- Prob/testcase_maker.js
+++ Prob/testcase_maker.js
@@ -1,10 +1,25 @@
 const fs = require("fs");
 let writeobj = [];
 
-for(let i = 1; i <= 100; i++){
+for(let i = 1; i <= 30; i++){
+    let a = Math.floor(Math.random() * 400);
+    let b = Math.floor(Math.random() * 400);
+    let c = Math.floor(Math.random() * 400);
+
+    let ans = 0;
+    for(let i = 1; i <= a; i++){
+        for(let j = 1; j <= b; j++){
+            for(let k = 1; k <= c; k++){
+                if(i * j * k % 18 == 0) ans++;
+            }
+        }
+    }
+
+    let e = a + " " + b + " " + c;
+    
     writeobj.push(
-        {"input": i,
-        "output": i * 206}
+        {"input": e,
+        "output": ans}
     );
 }
-fs.writeFileSync("..\\Testcases\\0002.json", JSON.stringify(writeobj));
+fs.writeFileSync("..\\Testcases\\0004.json", JSON.stringify(writeobj));
