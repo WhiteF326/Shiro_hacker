54d6a19fdda323f371f6853f339b66229d35cb40
WhiteF326 <Lume1a326.leno@gmail.com>
2021-04-19 15:05:12 +0900

    next:make problems.html

--- Server.js
+++ Server.js
@@ -1,22 +1,35 @@
 // server.js
 
-import {Server} from "https://js.sabae.cc/Server.js"
+import { Server } from "https://js.sabae.cc/Server.js"
 
-class body extends Server{
-  async api(path, prm){
+class body extends Server {
+  async api(path, prm) {
     let retobj = null;
-    switch(path.split("/")[2]){
+    switch (path.split("/")[2]) {
       case "":
         break;
 
+      //コンテスト情報の取得
       case "contests":
-        if(path.split("/")[3] == "list"){
+        if (path.split("/")[3] == "list") {
           retobj = [];
-          for await(const dirEntry of Deno.readDir("Contests")){
+          for await (const dirEntry of Deno.readDir("Contests")) {
             retobj.push(dirEntry);
           }
-        }else if(path.split("/")[3] == "search"){
+        } else if (path.split("/")[3] == "search") {
           retobj = await Deno.readTextFile("./Contests/" + prm.name);
+        }
+        break;
+
+      //問題情報の取得
+      case "problems":
+        if (path.split("/")[3] == "list") {
+          retobj = [];
+          for await (const dirEntry of Deno.readDir("Problems")) {
+            retobj.push(dirEntry);
+          }
+        } else if (path.split("/")[3] == "search") {
+          retobj = await Deno.readTextFile("./Problems/" + prm.name);
         }
         break;
 
